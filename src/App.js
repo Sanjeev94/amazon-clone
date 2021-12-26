9e7425f6a48fd6364673c8e75253289b4d3089e6
Sanjeev94 <sanjeev.dubey930@gmail.com>
2021-12-26 16:13:45 +0530

    adding login and register

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import { Switch, BrowserRouter, Route, } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Header from './components/Header/Header';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Switch>
+      <Route path="/register" >
+          <Register />
+        </Route>
+      <Route path="/login" >
+          <Login />
+        </Route>
         <Route path="/" >
           <Header />
           <Home />
