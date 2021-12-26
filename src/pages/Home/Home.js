82e11d649ffc6dbb135d1b5bc23fe9e1eaf11f28
Sanjeev94 <sanjeev.dubey930@gmail.com>
2021-12-26 07:16:45 +0530

    adding product and backtotop

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,6 +8,8 @@ import Banner4 from "../../BannerImages/Banner4.jpg";
 import Banner5 from "../../BannerImages/Banner5.jpg";
 import Banner6 from "../../BannerImages/Banner6.jpg";
 import Slider from "../../components/Slider/Slider";
+import Product from '../../components/Product/Product';
+import BackToTop from "../../components/BackToTop/BackToTop";
 
 const Home = () => {
 
@@ -23,9 +25,51 @@ const Home = () => {
                     <Slider images={bannerImages} />
                     <div className="home-row">
                         {/* products */}
+                        {products.slice(0,2).map((item) => (
+                            <Product 
+                            key={item.id} 
+                            id={item.id}
+                            title={item.title} 
+                            price={item.price}
+                            rating={item.rating}
+                            image={item.image}
+                            specification={item.specification}
+                            detail={item.detail}
+                            />
+                        ))}
+                    </div>
+                    <div className="home-row">
+                        {/* products */}
+                        {products.slice(2,5).map((item) => (
+                            <Product 
+                            key={item.id} 
+                            id={item.id}
+                            title={item.title} 
+                            price={item.price}
+                            rating={item.rating}
+                            image={item.image}
+                            specification={item.specification}
+                            detail={item.detail}
+                            />
+                        ))}
+                    </div>
+                    <div className="home-row">
+                        {/* products */}
+                        {products.slice(5,6).map((item) => (
+                            <Product 
+                            key={item.id} 
+                            id={item.id}
+                            title={item.title} 
+                            price={item.price}
+                            rating={item.rating}
+                            image={item.image}
+                            specification={item.specification}
+                            detail={item.detail}
+                            />
+                        ))}
                     </div>
                     <div style={{marginTop: "40px"}}>
-                        {/* back to top */}
+                        <BackToTop />
                     </div>
                 </div>
             </div>
